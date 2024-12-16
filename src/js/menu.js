5cc929b5ea5b61a6d846fd4538086fde36f57b50
YoussefBinks25 <youssef.khadhraoui@edu.univ-fcomte.fr>
2024-12-16 10:56:52 +0100

    réglages pour que le menu soit correct

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,11 +1,13 @@
-const menuToggle = document.getElementById('menu-toggle');
-const menuClose = document.getElementById('menu-close');
-const menu = document.getElementById('menu');
+const menuToggle = document.getElementById("menu-toggle");
+const menuClose = document.getElementById("menu-close");
+const menu = document.getElementById("menu");
 
-menuToggle.addEventListener('click', () => {
-    menu.classList.add('active');
+// Open Menu
+menuToggle.addEventListener("click", () => {
+    menu.classList.add("active");
 });
 
-menuClose.addEventListener('click', () => {
-    menu.classList.remove('active');
+// Close Menu
+menuClose.addEventListener("click", () => {
+    menu.classList.remove("active");
 });
